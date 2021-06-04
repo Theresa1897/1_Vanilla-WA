a0e3c308d7d752059bd1e367c877b18b6c6717a6
Theresa1897 <82607715+Theresa1897@users.noreply.github.com>
2021-06-04 14:57:16 -0700

    updated time and removed console.log

--- 1_Vanilla-weatherApp/src/app.js
+++ 1_Vanilla-weatherApp/src/app.js
@@ -1,15 +1,40 @@
+funtion formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = timestamp.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateEmelent = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateEmelent.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let asiKey = "f31f167d6e8ea7b22fba3adb5bdbb2b7";
