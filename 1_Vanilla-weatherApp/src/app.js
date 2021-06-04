b5d0c616dec98cc3c70f6533750b2e2e65209e97
Theresa1897 <82607715+Theresa1897@users.noreply.github.com>
2021-06-04 16:12:51 -0700

    Added search engine

--- 1_Vanilla-weatherApp/src/app.js
+++ 1_Vanilla-weatherApp/src/app.js
@@ -40,8 +40,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let asiKey = "f31f167d6e8ea7b22fba3adb5bdbb2b7";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+function search(city) {
+let apiKey = "f31f167d6e8ea7b22fba3adb5bdbb2b7";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
